5b4ac4c096678793a6e4a887d0312ba5bccc3039
existence-mp3 <existence.mp3@gmail.com>
2026-03-19 18:09:15 -0700

    moving my experimentation to seperate repository, new elastic layout

--- src/main/java/frc/robot/subsystems/drivetrain/Swerve.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.subsystems.drivetrain;
-
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-
-public class Swerve {
-
-    private final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(SwerveConstants.getModuleLocations());
-
-}
